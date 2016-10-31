597dcd6210bd0f44d07bb7ccaf1ae8db3b28af6b
xuwei <syxyxuwei@163.com>
2016-10-31 19:11:49 +0800

    增加MainActivity注释

--- volleydemo/src/main/java/com/leador/volleydemo/MainActivity.java
+++ volleydemo/src/main/java/com/leador/volleydemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.leador.volleydemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * volley网络请求框架的使用
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
