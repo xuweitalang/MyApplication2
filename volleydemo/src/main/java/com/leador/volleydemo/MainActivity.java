fcc5681ff6c56ff06963075112e69790426176f2
xuwei <syxyxuwei@163.com>
2016-11-21 20:53:28 +0800

    提交volley测试代码

--- volleydemo/src/main/java/com/leador/volleydemo/MainActivity.java
+++ volleydemo/src/main/java/com/leador/volleydemo/MainActivity.java
@@ -2,6 +2,13 @@ package com.leador.volleydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 /**
  * volley网络请求框架的使用
@@ -12,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //创建一个请求队列对象
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+//        创建一个StringRequest对象
+        StringRequest sr = new StringRequest("http://www.baidu.com", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                Toast.makeText(MainActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(MainActivity.this,"登录失败",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(sr); //将StringRequest对象添加到RequestQueue里面
     }
 }
